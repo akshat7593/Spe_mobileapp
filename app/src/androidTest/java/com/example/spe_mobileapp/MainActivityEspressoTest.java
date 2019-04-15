24f3c8edd3210e583acdea9217d552e22167e71c
akshat7593 <akshat.agrawal@iiitb.org>
2019-04-15 10:04:19 +0530

    wildcard changed

---
+++ app/src/androidTest/java/com/example/spe_mobileapp/MainActivityEspressoTest.java
@@ -0,0 +1,44 @@
+package com.example.spe_mobileapp;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityEspressoTest {
+
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void ensureTextChangesWork() {
+        // Type text and then press the button.
+        onView(withId(R.id.login_roll))
+                .perform(typeText("MT2018007"), closeSoftKeyboard());
+
+        onView(withId(R.id.login_pass))
+                .perform(typeText("100100100"), closeSoftKeyboard());
+        onView(withId(R.id.data_login)).perform(click());
+
+        //onView(withId(R.id.data_login)).check()
+
+    }
+
+
+}
+
